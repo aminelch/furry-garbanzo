d57e2ff99a542db1b2b449c983c528253962dc67
aminelch <aminelch95@gmail.com>
2021-03-03 19:03:21 +0100

    mise à jour de script set-command&create-var

--- variable-env/create-envar.sh
+++ variable-env/create-envar.sh
@@ -5,5 +5,7 @@ echo $MESSAGE_A
 
 export MESSAGE_A 
 
+# la commande set sans argument affiche (entre autres) toutes les variables associées à un processus
+# la commande env affiche uniquement les variables d'environnements
 echo "essayer d'afficher la variable MESSAGE_A endehors de ce script" 
 exit;
---
+++ variable-env/set-command.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+#commande set  sans argument affiche (entre autres) toutes les variables associées à un processus
+
+set 
