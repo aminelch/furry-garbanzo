47950e6041009821545a339e318c1fa7442aeb75
aminelch <aminelch95@gmail.com>
2021-03-03 19:02:49 +0100

    mise à jour de script word-count

--- nombre-mots/word-count.sh
+++ nombre-mots/word-count.sh
@@ -3,5 +3,13 @@
 echo "Entrer une suite des mots" 
 read suite 
 
+# sauvgarde de la valeur de IFS
+svIFS=$IFS 
+
+# ifs devient un espace 
+IFS=" "
+
+set $suite 
+
 echo "Le nombre des mots est $# " 
 
