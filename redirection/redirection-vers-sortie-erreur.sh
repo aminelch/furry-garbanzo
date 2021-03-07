ced27023b26aff108c03b86d9cd73ae894e9b0f9
aminelch <aminelch95@gmail.com>
2021-03-07 16:02:49 +0100

    redirection des erreurs d une commande

---
+++ redirection/redirection-vers-sortie-erreur.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+echo "voir le cotenu de fichier output-error "
+echo "redirection des messages d'erreurs d'une commande vers un fichier"
+echo "la commande ls vim  2> output-error" 
+
+ls vim  2> output-error 
+
